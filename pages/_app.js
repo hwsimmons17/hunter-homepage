54c48656633b144b3f1091c8571e3ce7e4ac6d57
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2021-06-10 18:54:23 -0600

    google-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
-import Header from "../components/Header";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 import Navbar from "../components/Navbar";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Navbar />
