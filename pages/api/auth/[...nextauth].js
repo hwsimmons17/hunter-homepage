07630b5d250dff599537965aef61a838da21edc5
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2021-06-11 10:48:43 -0600

    trying oauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,8 +8,8 @@ export default NextAuth({
       clientId:
         "1028384161214-s9dndgfdm2lvu0talvg4684us69fm813.apps.googleusercontent.com",
       clientSecret: process.env.GOOGLE_SECRET,
-      authorizationUrl:
-        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
+      //   authorizationUrl:
+      //     "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
     }),
     // ...add more providers here
   ],
