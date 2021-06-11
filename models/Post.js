18a47eaa1c95114b75bda9959df4927b9d61dd86
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2021-06-11 16:39:06 -0600

    added data to api

--- models/Post.js
+++ models/Post.js
@@ -20,6 +20,10 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  date: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.models.Post || mongoose.model("Post", PostSchema);
